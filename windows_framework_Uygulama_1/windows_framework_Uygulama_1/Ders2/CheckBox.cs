5042bfd36dbb058a81156872b0702744c3b9acf7
Amjed Hezeny <140171298+AmjedHezeny@users.noreply.github.com>
2023-10-07 00:06:07 +0300

    Add files via upload

---
+++ windows_framework_Uygulama_1/windows_framework_Uygulama_1/Ders2/CheckBox.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace windows_framework_Uygulama_1
+{
+    public partial class CheckBox : Form
+    {
+        public CheckBox()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked  ) 
+            {
+                label1.Text = "CheckBox Seçili";
+                label1.ForeColor = Color.Green;
+            }
+            else
+            {
+                label1.Text = "CheckBox Seçili DEĞİL";
+                label1.ForeColor = Color.Red;
+            }
+        }
+    }
+}
---
+++ windows_framework_Uygulama_1/windows_framework_Uygulama_1/Ders2/ListBox_Odev.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ders2
+{
+    public partial class ListBox_Odev : Form
+    {
+        public ListBox_Odev()
+        {
+            InitializeComponent();
+        }
+
+        //Ekle butonuna basıldığında Textbox ın içerisinde yazan değeri ListBox a ekleme İşlemi yapalım
+        //Düzenle butonuna basıldığın ListBox dan hangi değer seçmişse onu texbox ın içerisinde yazan değerle değiştireli
+        //Sil butonuna basınca seçilen değeri silelim
+        //Hepsini Sil e basınca kayıtlı bütün verileri silelim
+        //Sırala Butonuna basınca listbox ın içerisindeki verileri alfabetik olarak sıralama işlemi yapsın
+    }
+}
